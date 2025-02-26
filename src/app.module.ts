1d03085464577e8d00683e799d1dce76ac6072b6
Tirth-DevOps <tirth11.cilans@gmail.com>
2025-02-26 15:21:51 +0530

    refactor: remove unused module imports from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { AppService } from './app.service';
 import { MarketAnalysisController } from './market-analysis/market-analysis.controller';
 import { MarketAnalysisGateway } from './market-analysis/market-analysis.gateway';
 import { MarketAnalysisService } from './market-analysis/market-analysis.service';
-import { OrderModule } from './order/order.module';
+// import { OrderModule } from './order/order.module';
 // import { DatabaseModule } from './database/database.module';
-import { NotificationModule } from './notification/notification.module';
+// import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [OrderModule, NotificationModule],
+  imports: [],
   controllers: [AppController, MarketAnalysisController],
   providers: [AppService, MarketAnalysisGateway, MarketAnalysisService],
 })
